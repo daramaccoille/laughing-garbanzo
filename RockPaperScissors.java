c65dd29fd57c3b9c0870d1efe37283618ab0af03
daramaccoille <59489287+daramaccoille@users.noreply.github.com>
2023-11-14 14:17:16 +0000

    Add files via upload

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,113 +1,131 @@
 /**
  * 
  */
-package week6_reading;
+package RockPaperScissors;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-import javax.swing.text.StyledEditorKit.ForegroundAction;
-
 /**
- * @author Dara Mac Coille
- * @version Nov 4, 2023
+ * @author Dara Mac Coille daramaccoille@gmail.com
+ * @version 01/11/2023
  */
 public class RockPaperScissors {
 
 	/**
 	 * @param args
-	 * @throws InterruptedException
 	 */
-	public static void main(String[] args) throws InterruptedException {
-		String[] answers = new String[] { "Rock", "Paper", "Scissors" };
-		Scanner scanner = new Scanner(System.in);
-		// Ask user to change rounds
+	public static void main(String[] args) {
+		// variables
 		int rounds = 5;
 		int playerScore = 0;
 		int computerScore = 0;
 		int drawScore = 0;
-		System.out.println("Would you like to change the number of rounds from 5? Enter \"y\" or \"n\"");
-		if (scanner.next().equalsIgnoreCase("y")) {
-			System.out.println("OK.. enter the number of rounds you'd like to play now: ");
+		String changeRounds;
+		String[] guesses = new String[] { "rock", "paper", "scissors" };
+		Scanner scanner = new Scanner(System.in);
+		
+		// Print header and collect change rounds answer
+		System.out.println("Welcome, would you like to change rounds from " + rounds + " 'Y/N'?");
+		changeRounds = scanner.next();
+
+		// collect new number of rounds
+		if (changeRounds.equalsIgnoreCase("Y")) {
+			System.out.println("OK enter the number of rounds to play now: ");
 			rounds = scanner.nextInt();
+			System.out.println("OK let's play "+rounds+" rounds");
+			scanner.nextLine();
 		}
+		else {
+			
+			scanner.nextLine();
+		}
+		
+		// Iterate over the number of rounds
 		for (int i = 1; i <= rounds; i++) {
-			System.out.println("Round " + i + " Please enter \"Rock\",\"Paper\" or \"Scissors");
+
+			// Guesses
+			System.out.println("Round " + i + " Please write \"Rock\",\"Paper\", "
+					+ "or \"Scissors\":");
+			
+			// Random guess for computer
 			Random random = new Random();
-			int intComputerGuess = random.nextInt(3);
-			String stringComputerAnswer = answers[intComputerGuess];
-			String userInput = scanner.next();
-			while (!isValidAnswer(userInput, answers)) {
-				System.out.println("Please enter \"Rock\",\"Paper\",\"Scissors\"");
-				userInput = scanner.next();
+			int computerIntGuess = random.nextInt(3);
+			String computerStringGuess = guesses[computerIntGuess];
+			
+			// Collect user guess
+			String playerUnvalidatedGuess = scanner.nextLine();
+			// Validate user input
+			boolean validAnswer = validatePlayerGuess(playerUnvalidatedGuess.toLowerCase(),guesses);
+			 while (validAnswer==false)
+			{
+				 System.out.println("Sorry "+playerUnvalidatedGuess+" wasn't right. Please write \"Rock\",\"Paper\", or \"Scissors\":");
+			playerUnvalidatedGuess = scanner.nextLine();
+			validAnswer=validatePlayerGuess(playerUnvalidatedGuess.toLowerCase(),guesses);
 			}
-			try {
-				// Check user input
-				System.out.println("You picked " + userInput);
-				if (isValidAnswer(userInput, answers)) {
-					System.out.print("Computer picks ");
-					for (int j = 0; j < 3; j++) {
-						System.out.print(".");
-						Thread.sleep(700);
-					}
-					System.out.print(" "+stringComputerAnswer+" ");
-
-					if (isPlayerWin(userInput, stringComputerAnswer)) {
-						playerScore += 1;
-						System.out.println("Player wins your score is " + playerScore);
-					} else if (isComputerWin(userInput, stringComputerAnswer)) {
-						computerScore += 1;
-						System.out.println("Compuer wins their score is " + computerScore);
-					} else {
-						drawScore += 1;
-						System.out.println("Draw .... draw score is " + drawScore);
-					}
+				// Calculate if player has won
+				if (playerUnvalidatedGuess.equalsIgnoreCase("rock") && computerStringGuess.equals("scissors")
+						|| playerUnvalidatedGuess.equalsIgnoreCase("paper") && computerStringGuess.equals("rock")
+						|| playerUnvalidatedGuess.equalsIgnoreCase("scissors") && computerStringGuess.equals("paper")) {
+					playerScore += 1;
+					System.out.println("Computer chose " + computerStringGuess + " and you" + " chose "
+							+ playerUnvalidatedGuess + " so you have gained a point!");
+					printScore(computerScore, playerScore, drawScore);
 				}
-			} catch (Exception e) {
-				System.out.println("Your choice wasn't valid. Please enter \"Rock\",\"Paper\" or \"Scissors ");
-			}
+
+				// Calculate if computer has won
+				else if (playerUnvalidatedGuess.equalsIgnoreCase("scissors") && computerStringGuess.equals("rock")
+						|| playerUnvalidatedGuess.equalsIgnoreCase("rock") && computerStringGuess.equals("paper")
+						|| playerUnvalidatedGuess.equalsIgnoreCase("paper") && computerStringGuess.equals("scissors")) {
+					computerScore += 1;
+					System.out.println("Computer chose " + computerStringGuess + " and you" + " chose "
+							+ playerUnvalidatedGuess + " so computer has gained a point!");
+					printScore(computerScore, playerScore, drawScore);
+				}
+				// Calculate if it's a draw
+				else {
+					drawScore += 1;
+					System.out.println("Computer chose " + computerStringGuess + " and you" + " chose "
+							+ playerUnvalidatedGuess + " so no one gets a point!");
+					printScore(computerScore, playerScore, drawScore);
+				}
+			
 		}
-		printStats(playerScore,computerScore,drawScore,rounds);
+		//scanner.close();
+		printGameStats(computerScore, playerScore, drawScore,rounds);
+	} 
+
+	public static void printScore(int computerScore, int playerScore, int drawScore) {
+		System.out.println("--------------------------------------------------------------");
+		System.out.println(
+				"The scores are \tPlayer: " + playerScore + "\tComputer: " + computerScore + "\tDraws: " + drawScore);
+		System.out.println("--------------------------------------------------------------");
 	}
 
-	public static boolean isPlayerWin(String playerGuess, String computerGuess) {
-		if (playerGuess.equalsIgnoreCase("Rock") && computerGuess.equalsIgnoreCase("Scissors")) {
-			return true;
-		} else if (playerGuess.equalsIgnoreCase("Paper") && computerGuess.equalsIgnoreCase("Rock")) {
-			return true;
-		} else if (playerGuess.equalsIgnoreCase("Scissors") && computerGuess.equalsIgnoreCase("Paper")) {
-			return true;
-		} else
-			return false;
+	/**
+	 * 
+	 * @param playerUnvalidatedGuess is the String guess collected from the scanner
+	 * @param guesses                is the String array that holds rock, paper and
+	 *                               scissors
+	 * @return a boolean if the scanner string is in the guesses array
+	 */
+	public static boolean validatePlayerGuess(String playerUnvalidatedGuess, String[] guesses) {
+		boolean validGuess;
+		validGuess=Arrays.asList(guesses).contains(playerUnvalidatedGuess)?true:false;
+		return validGuess;
+	}
+	public static void printGameStats(int compScore, int playScore, int draws, int rounds) {
+		double playerPercentage=((double)playScore/(double)rounds)*100;
+		double computerPercentage=((double)compScore/(double)rounds)*100;
+		double drawPercentage=((double)draws/(double)rounds)*100;
+		
+		System.out.println("----------------------------------------------------------");
+		System.out.println("--------Thanks for playing, here's the stats--------------");
+		System.out.println();
+		System.out.printf("Player percentage win :\t\t %.2f\n",playerPercentage);
+		System.out.printf("Computer percentage win :\t %.2f\n",computerPercentage);
+		System.out.printf("Draw percentage win :\t\t %.2f\n",drawPercentage);
 	}
 
-	public static boolean isComputerWin(String playerGuess, String computerGuess) {
-		if (playerGuess.equalsIgnoreCase("Rock") && computerGuess.equalsIgnoreCase("Paper")) {
-			return true;
-		} else if (playerGuess.equalsIgnoreCase("Paper") && computerGuess.equalsIgnoreCase("Scissors")) {
-			return true;
-		} else if (playerGuess.equalsIgnoreCase("Scissors") && computerGuess.equalsIgnoreCase("Rock")) {
-			return true;
-		} else
-			return false;
-	}
-
-	public static boolean isValidAnswer(String userAnswer, String[] answers) {
-		for (String string : answers) {
-			if (string.equalsIgnoreCase(userAnswer)) {
-				return true;
-			}
-		}
-		System.out.println("You picked wrong with " + userAnswer + " try again ");
-		return false;
-	}
-
-	public static void printStats(int playerScore, int computerScore, int drawScore, int rounds) {
-		// Print statistics out
-		System.out.println("------------------------Statistics---------------------------\n");
-		System.out.println("Player percentage: " + (double) playerScore / rounds * 100 + "%");
-		System.out.println("Computer percentage: " + (double) computerScore / rounds * 100 + "%");
-		System.out.println("Draw percentage: " + (double) drawScore / rounds * 100 + "%");
-	}
 }
